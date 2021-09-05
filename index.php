7fc5ab10596d8032f1a48c7eeb83f70e700c8db6
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2021-09-05 20:06:05 +0700

    v9.5

---
+++ index.php
@@ -0,0 +1,222 @@
+<?php 
+
+# // test comment 
+
+/*
+test  
+comment 
+*/
+
+   // echo ('Test Message');
+   // print('Test message');
+
+//    ECHO ('Test Message');
+
+
+
+
+
+//    $name = "ahmed";
+//    $age = 20;
+
+
+//    $name = "Root";
+
+//     echo $name;
+
+//var_dump($age);
+
+
+
+
+//  define("size",50,true);     // case-insensitive (false)
+
+// //  echo size;
+
+//  echo constant('SIZE');
+
+
+/*
+ +
+ -
+ /
+ *
+ %
+**
+*/
+
+
+
+  // $num1 = 10;
+// $num2 = 20;
+
+  // echo $result = $num1 + $num2;
+
+  // echo   $num1 += 3;     //  $num1 + 3;
+
+
+//   $num1 = '20';
+//   $num2 = 20;
+
+
+
+//   $num1 == $num2       ,    $num1 === $num2  
+
+//   $num1 != $num2     ,    $num1 !== $num2 
+  
+//   $num1 > $num2 <    , <= , >=  
+
+
+
+// $x = 2;
+
+// $x += 1; 
+
+// $x = $x+1;
+
+
+ // echo   $x++;  // post increment .    2  
+
+
+ //  echo ++$x;   // pre increment 
+
+
+
+ //echo $x--;    // 2 
+
+//  echo --$x;
+
+
+  ///  and = &&    ,   or   =  || 
+
+//    cond1    op      cond2 
+//    true     and (&&)     true      >>> true     
+//    true     and     false     >>> false 
+//    false    and     true      >>> false 
+//    false    and     false     >>> false 
+
+
+//    cond1    op     cond2   
+//    true     or  (||)     true   >>> true 
+//    true     or      false  >>> true 
+//    false    or      true   >>> true 
+//    false    or      false  >>> false 
+
+
+//    cond1    op      cond2
+//    true     xor      true   >>>   false 
+//    true     xor      false  >>>   true 
+//    false    xor      true   >>>   true 
+//    false    xor      false  >>>   false 
+
+//   $age = 30;
+//    !($age > 20)  // false 
+
+
+//   $message = "Welcome to  ";
+//   $name = "Root";
+
+
+
+//   $name .= " X";
+
+
+//     echo   $text = $message.$name; 
+
+
+// $age = 17;
+
+
+// if( ($age > 15)  && ($age <= 20)){
+
+//     echo 'You Age > 15 && Age <=20';
+
+// }elseif(($age > 20) && ($age <= 30)){
+   
+//     echo 'You Age > 20 && Age <=30';
+
+// }else{
+//      echo 'You Age > 30 || Age <= 15';
+// }
+
+
+
+
+// $color = "red";
+
+//      switch ($color) {
+         
+//          case 'red':
+//              # code...
+//              echo 'Color Is Red';
+//              break;
+
+//          case 'blue': 
+//             echo 'Color Is Blue';    
+//              break;
+
+//          default:
+//              # code...
+//              echo 'No Color';
+//              break;
+//      }
+
+
+
+// $age = 20;
+
+// switch ($age) {
+//     case ($age >= 20):
+//         # code...
+    
+//          echo 'AGE >= 20';
+
+//         break;
+    
+//     default:
+//         # code...
+//         break;
+// }
+
+
+
+
+// + 
+// -
+// / 
+
+
+
+$op = "-";
+$num1 = 1;
+$num2 = 2;
+
+ switch ($op) {
+     case '+':
+         # code...
+         echo $num1 + $num2;
+         break;
+
+     case '-':
+        # code...
+        echo $num1-$num2;     
+        break;
+        
+     default:
+         # code...
+         echo 'No OPERATION SET ';
+         break;
+ }
+
+
+
+
+
+
+
+
+
+
+?>
+
+ 
